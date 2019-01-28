ea8317a08cd4e642134824127bd477c93163c285
Artem <artem.volshynskyi@gmail.com>
2019-01-28 14:53:43 +0200

    Correct messages_room_id

--- chat/rest_views.py
+++ chat/rest_views.py
@@ -10,7 +10,11 @@ async def messages_id(request):
 
 
 async def messages_room_id(request):
-    room_id = int(request.match_info.get('id', 2))
-    async with request.app['models'].acquire() as conn:
-        record = await get_messages_with_users_by_room_id(conn, room_id)
-    return web.json_response(record)
+    try:
+        room_id = int(request.match_info.get('id', 1))
+        async with request.app['models'].acquire() as conn:
+            record = await get_messages_with_users_by_room_id(conn, room_id)
+        return web.json_response(record)
+    except ValueError:
+        raise web.HTTPBadRequest
+
